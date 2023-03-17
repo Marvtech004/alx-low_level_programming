99f0f7c2574cf421eb1673b9e5b4178a70eacc73
Marvtech004 <aderonmuna@gmail.com>
2023-03-17 04:50:36 -0700

    udpdate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,62 +1,47 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
-* _strlen - finds the length of string
-* @str: string
-*
-* Return: length of the string
-*/
-
-int _strlen(char *str)
-{
-	int len = 1;
-
-	while (str[len] != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
-* string_nconcat - concatenates two strings only n bytes of s2
-* @s1: first string
-* @s2: second string
-* @n: number of bytes specified
-*
-* Return: NULL if fail, else pointer to malloc memory
-*/
-
+ * string_nconcat - a function that concatenates two strings.
+ *
+ * @s1: first char
+ * @s2: secound char
+ * @n: unsigned int
+ *
+ * Return: If the function fails, it should return NULL
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int len1, len2, i, j;
+	unsigned int x, y, z;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-
-	if (n >= len2)
-		n = len2;
-
-	ptr = malloc(len1 + n + 1);
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0, j = 0; i < (len1 + n); i++)
 	{
-		if (i < len1)
-			ptr[i] = s1[i];
-		else
-			ptr[i] = s2[j++];
-
+		x = 0;
 	}
-	ptr[i] = '\0';
-	return (ptr);
+	else
+	{
+		for (x = 0; s1[x]; ++x)
+		;
+	}
+	if (s2 == NULL)
+	{
+		y = 0;
+	}
+	else
+	{
+		for (y = 0; s2[y]; ++y)
+		;
+	}
+	if (y > n)
+		y = n;
+	s = malloc(sizeof(char) * (x + y + 1));
+	if (s == NULL)
+		return (NULL);
+	for (z = 0; z < x; z++)
+		s[z] = s1[z];
+	for (z = 0; z < y; z++)
+		s[z + x] = s2[z];
+	s[x + y] = '\0';
+	return (s);
 }
 
