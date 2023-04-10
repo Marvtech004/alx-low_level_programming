90378d30a5d008380cda96dd0849cdd8d65c31a7
Marvtech004 <aderonmuna@gmail.com>
2023-04-10 00:44:19 -0700

    0-binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,20 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth:Marvtech
- * Decription:Header file contain prototypes for all functions
- * written in the 0x14 bit manipulation directory
- */
-
-
+int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
+void _divide(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
 
-#endif /* MAIN_H
+#endif
